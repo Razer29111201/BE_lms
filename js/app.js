619ec6da8757a3bde81381bec3991daf1e9e8685
Razer29111201 <121886578+Razer29111201@users.noreply.github.com>
2025-12-25 16:11:21 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 // ===========================================
 // CONFIG
 // ===========================================
-const API_BASE = 'http://localhost:8080/api';
+const API_BASE = 'https://bee-army.onrender.com/api';
 
 // ===========================================
 // API MODULE
